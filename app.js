e9d7b0166d8aaf9a2f98b05341155bcea8d6d558
Divyanshi <dddixit333@gmail.com>
2025-07-17 12:41:41 +0530

    Added changes

--- app.js
+++ app.js
@@ -84,15 +84,9 @@ app.use((req , res , next) => {
     next() ; 
 }) ; 
 
-app.get("/demouser" , async(req ,res) => {
-    let fakeUser = new User({
-        email : "student@gmail.com" , 
-        username : "delta-student"
-    }) ; 
-
-    let registeredUser = await User.register(fakeUser , "helloworld") ; 
-    res.send(registeredUser) ; 
-})
+app.get("/", (req, res) => {
+    res.render("home.ejs"); // make sure you have views/home.ejs
+  });
 
 app.use("/listings" , listingRouter) ; 
 app.use("/listings/:id/reviews" , reviewRouter) ; 
